1e4aedf4131d8db3a6141749ba5c04c176e65db6
Nayeem Hassan <nayeemhasan042@gmail.com>
2024-09-03 16:56:23 +0600

    'props'

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <p>This is a footer.</p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+const Header = () => {
+    
+    return (
+        <div>
+            <h1>Login Status</h1>
+        </div>
+    );
+
+
+};
+
+export default Header;
