383d9b41834029a87156a1bfc62e942a466fe2d2
Nayeem Hassan <nayeemhasan042@gmail.com>
2024-09-03 19:19:15 +0600

    object

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import Footer from './components/Footer';
 import Hero from './components/Hero'
 
 const App = () => {
+  const itemObj = {
+    name:'nayeem',
+    age: 23,
+    city: 'dhaka'
+  }
   return (
     <div>
-      <Header/>
+      <Header item={itemObj} />
       <Footer/>
       <Hero title="React" des="We love React" />
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 
 
-const Header = () => {
+const Header = (props) => {
     
     return (
         <div>
             <h1>Login Status</h1>
+            <ul>
+                <li>Name : {props.item.name}</li>
+                <li>Age : {props.item.age}</li>
+                <li>City : {props.item.city}</li>
+            </ul>
         </div>
     );
 
